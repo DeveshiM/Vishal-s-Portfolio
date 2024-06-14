24fb46bdf0d709dc43462537302c49e0de0aa894
DeveshiM <deveshi.maheshwari@metaorangedigital.com>
2024-06-14 16:00:31 +0530

    organized

--- src/Component/ourClient.jsx
+++ src/Component/ourClient.jsx
@@ -1,19 +1,19 @@
 import react from 'react';
 import img1 from '../assets/image/image-1.png'
-import img2 from '../assets/image/image-1.png'
-import img3 from '../assets/image/image-1.png'
-import img4 from '../assets/image/image-1.png'
-import img5 from '../assets/image/image-1.png'
-import img6 from '../assets/image/image-1.png'
-import img7 from '../assets/image/image-1.png'
-import img8 from '../assets/image/image-1.png'
-import img9 from '../assets/image/image-1.png'
-import img10 from '../assets/image/image-1.png'
-import img11 from '../assets/image/image-1.png'
-import img12 from '../assets/image/image-1.png'
-import img13 from '../assets/image/image-1.png'
-import img14 from '../assets/image/image-1.png'
-import img15 from '../assets/image/image-1.png'
+import img2 from '../assets/image/image-2.png'
+import img3 from '../assets/image/image-3.png'
+import img4 from '../assets/image/image-4.jpg'
+import img5 from '../assets/image/image-5.jpg'
+import img6 from '../assets/image/image-6.png'
+import img7 from '../assets/image/image-7.png'
+import img8 from '../assets/image/image-8.jpg'
+import img9 from '../assets/image/image-9.jpg'
+import img10 from '../assets/image/image-10.png'
+import img11 from '../assets/image/image-11.png'
+import img12 from '../assets/image/image-12.png'
+import img13 from '../assets/image/image-13.png'
+import img14 from '../assets/image/image-14.png'
+import img15 from '../assets/image/image-15.png'
 
  function client(){
     return (
@@ -23,57 +23,57 @@ import img15 from '../assets/image/image-1.png'
                     <div>
                         <h1 className='py-10 text-[#3b6893] text-5xl text-center font-bold'>Our Clients</h1>
                     </div>
-                    <div className='flex p-8 text-center'>
-                        <div>
-                            <img src={img1} alt="" />
+                    <div className='flex p-8 text-center justify-between'>
+                        <div  >
+                            <img src={img1} alt="" className='h-16 w-40'/>
                         </div>
-                        <div>
-                            <img src={img2} alt="" />
+                        <div  >
+                            <img src={img2} alt="" className='h-16 w-40' />
                         </div>
-                        <div>
-                            <img src={img3} alt="" />
+                        <div  >
+                            <img src={img3} alt="" className='h-16 w-40'/>
                         </div>
-                        <div>
-                            <img src={img4} alt="" />
+                        <div  >
+                            <img src={img4} alt="" className='h-16 w-40'/>
                         </div>
-                        <div>
-                            <img src={img5} alt="" />
-                        </div>
-
-                    </div>
-                    <div className='flex p-8 text-center'>
-                        <div>
-                            <img src={img6} alt="" />
-                        </div>
-                        <div>
-                            <img src={img7} alt="" />
-                        </div>
-                        <div>
-                            <img src={img8} alt="" />
-                        </div>
-                        <div>
-                            <img src={img9} alt="" />
-                        </div>
-                        <div>
-                            <img src={img10} alt="" />
+                        <div  >
+                            <img src={img5} alt="" className='h-16 w-40'/>
                         </div>
 
                     </div>
-                    <div className='flex p-8 text-center'>
-                        <div>
-                            <img src={img11} alt="" />
+                    <div className='flex p-8 text-center justify-between'>
+                        <div  >
+                            <img src={img6} alt="" className='h-16 w-40'/>
                         </div>
-                        <div>
-                            <img src={img12} alt="" />
+                        <div  >
+                            <img src={img7} alt="" className='h-16 w-40'/>
                         </div>
-                        <div>
-                            <img src={img13} alt="" />
+                        <div  >
+                            <img src={img8} alt="" className='h-16 w-40'/>
                         </div>
-                        <div>
-                            <img src={img14} alt="" />
+                        <div  >
+                            <img src={img9} alt="" className='h-16 w-40'/>
                         </div>
-                        <div>
-                            <img src={img15} alt="" />
+                        <div  >
+                            <img src={img10} alt="" className='h-16 w-40'/>
+                        </div>
+
+                    </div>
+                    <div className='flex p-8 text-center justify-between'>
+                        <div  >
+                            <img src={img11} alt="" className='h-16 w-40'/>
+                        </div>
+                        <div  >
+                            <img src={img12} alt="" className='h-16 w-40'/>
+                        </div>
+                        <div  >
+                            <img src={img13} alt="" className='h-16 w-40'/>
+                        </div>
+                        <div  >
+                            <img src={img14} alt="" className='h-16 w-40'/>
+                        </div>
+                        <div  >
+                            <img src={img15} alt="" className='h-16 w-40'/>
                         </div>
 
                     </div>
